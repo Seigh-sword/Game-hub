01c88c1c82d986587c01f6e7385e4a0a5b476d46
Arunkumar .s.j. <zack.yt.7085@gmail.com>
2025-11-09 18:31:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+// Fetch the list of approved games
+fetch('gamesList.json')
+  .then(response => response.json())
+  .then(games => {
+    const container = document.getElementById('gameCards');
+
+    games.forEach(game => {
+      // Create card element
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      // Add game info inside card
+      card.innerHTML = `
+        <h3>${game.name}</h3>
+        <p>By: ${game.author}</p>
+        <button>Play</button>
+      `;
+
+      // Button click loads game in iframe
+      card.querySelector('button').onclick = () => {
+        document.getElementById('gameFrame').src = `games/${game.folder}/index.html`;
+      };
+
+      // Add card to container
+      container.appendChild(card);
+    });
+  })
+  .catch(error => {
+    console.error('Error loading games:', error);
+  });
